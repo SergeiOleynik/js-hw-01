b927a93001067c8f7b53a7852a78b3083ee49892
SergeiOleynik <154737237+SergeiOleynik@users.noreply.github.com>
2024-01-24 10:40:55 +0200

    Add files via upload

---
+++ homeWorkTask1/js/task1.js
@@ -0,0 +1,9 @@
+const   nameG = "генератор";
+let     price =  1000;
+
+/* case 1:  price = 1000 */ 
+console.log(`обрано "${nameG}", ціна за одиницю ${price} кредитів`);
+
+/* case 2:  price = 2000 */
+price = 2000;
+console.log(`обрано "${nameG}", ціна за одиницю ${price} кредитів`);
